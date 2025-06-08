93a4744cb454f6fe4f76e1ee0cdd459c6366c13f
vesrntechnologies <vennjoy@gmail.com>
2025-06-08 19:27:33 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,607 @@
+let questions = [
+  {
+    numb: 1,
+    question: "How many pressure gauges are in regulator?",
+    immg: "img1",
+    answer: "Two pressure gauges",
+    options: [
+      "One pressure gauge",
+      "Two pressure gauges",
+      "Three pressure gauges",
+      "Four pressure gauges"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What procedure is followed to connect gas regulator to cylinder in gas welder?",
+    immg: "img2",
+    answer: "Open and close valve for few seconds",
+    options: [
+      "Open and close valve for few seconds",
+      "Open the valve for a longer period",
+      "No need to open the valve before connection",
+      "Open the valve for a few minutes of time"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which type of welding uses a non-ferrous filler with melting point above 450°C than base metal?",
+    immg: "img3",
+    answer: "Brazing",
+    options: [
+      "Brazing",
+      "Soldering",
+      "Arc welding",
+      "Gas welding"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which welding process enables to weld dissimilar metals?",
+    immg: "img4",
+    answer: "Brazing",
+    options: [
+      "Brazing",
+      "Seam welding",
+      "Spot welding",
+      "MIG welding"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is included the angle for a double bevel butt joint?",
+    immg: "img5",
+    answer: "45o",
+    options: [
+      "45o",
+      "35o",
+      "60o",
+      "20o"
+    ]
+  },
+  
+    {
+      numb: 6,
+      question: "What is the colour of calcium carbide?",
+      immg: "img6",
+      answer: "Dark - Grey",
+      options: [
+        "Dark - Red",
+        "Dark - Grey",
+        "Dark - Blue",
+        "Dark - Yellow"
+      ]
+    },
+    {
+      numb: 7,
+      question: "What is the symbol of oxygen?",
+      immg: "img7",
+      answer: "O2",
+      options: [
+        "O2",
+        "C2",
+        "H2O",
+        "C2H2"
+      ]
+    },
+    {
+      numb: 8,
+      question: "Which gas dissolves in acetone liquid?",
+      immg: "img8",
+      answer: "Acetylene",
+      options: [
+        "Oxygen",
+        "Nitrogen",
+        "Hydrogen",
+        "Acetylene"
+      ]
+    },
+    {
+      numb: 9,
+      question: "Which gas supports to combustion of fuels?",
+      immg: "img9",
+      answer: "Oxygen",
+      options: [
+        "Oxygen",
+        "Acetylene",
+        "Hydrogen",
+        "Carbon di oxide"
+      ]
+    },
+    {
+      numb: 10,
+      question: "Which document lists all the essential characteristics for performing a weld?",
+      immg: "img10",
+      answer: "Weld procedure specification",
+      options: [
+        "Weld procedure specification",
+        "Welder performance qualification",
+        "Welding procedure qualification",
+        "Welding procedure specification"
+      ]
+    },
+    {
+      numb: 11,
+      question: "What is the colour of acetylene cylinder?",
+      immg: "img11",
+      answer: "Maroon",
+      options: [
+        "Blue",
+        "Black",
+        "Yellow",
+        "Maroon"
+      ]
+    },
+    {
+      numb: 12,
+      question: "What is the name of the cylinder shown in the figure?",
+      immg: "img12",
+      answer: "Oxygen",
+      options: [
+        "Oxygen",
+        "Acetylene",
+        "Argon gas",
+        "Carbon-di-oxide"
+      ]
+    },
+    {
+      numb: 13,
+      question: "What is the name of the pressure system shown in figure?",
+      immg: "img13",
+      answer: "High",
+      options: [
+        "Low",
+        "High",
+        "Normal",
+        "Medium"
+      ]
+    },
+    {
+      numb: 14,
+      question: "What is the name of the pressure system shown in figure?",
+      immg: "img14",
+      answer: "Low",
+      options: [
+        "Low",
+        "High",
+        "Normal",
+        "Medium"
+      ]
+    },
+    {
+      numb: 15,
+      question: "What is the welding technique shown in the figure?",
+      immg: "img15",
+      answer: "Leftward",
+      options: [
+        "Leftward",
+        "Rightward",
+        "Backward",
+        "Upto downward"
+      ]
+    },
+    {
+      numb: 16,
+      question: "What is the angle of blowpipe in leftward technique?",
+      immg: "img16",
+      answer: "60o-70o",
+      options: [
+        "30o-40o",
+        "50o-60o",
+        "60o-70o",
+        "80o-90o"
+      ]
+    },
+    {
+      numb: 17,
+      question: "What is name of the brazing shown in figure?",
+      immg: "img17",
+      answer: "Furnace",
+      options: [
+        "Dip brazing",
+        "Torch",
+        "Furnace",
+        "Induction"
+      ]
+    },
+    {
+      numb: 18,
+      question: "Which welding defects have pin holes formed on the surface?",
+      immg: "img18",
+      answer: "Porosity",
+      options: [
+        "Spatter",
+        "Porosity",
+        "Blow holes",
+        "Burn through"
+      ]
+    },
+    {
+      numb: 19,
+      question: "What is the symbol of flat or down hand position?",
+      immg: "img19",
+      answer: "PA",
+      options: [
+        "PA",
+        "PC",
+        "PB",
+        "PD"
+      ]
+    },
+    {
+      numb: 20,
+      question: "What is the effect of arc blow?",
+      immg: "img20",
+      answer: "Poor fusion",
+      options: [
+        "Less spatters",
+        "Poor fusion",
+        "Good weld",
+        "Good penetration"
+      ]
+    },
+    {
+      numb: 21,
+      question: "What is defect in welded job to change its size and shape permanently?",
+      immg: "img21",
+      answer: "Distortion",
+      options: [
+        "Arc blow",
+        "Spatters",
+        "Distortion",
+        "Incomplete"
+      ]
+    },
+    {
+      numb: 22,
+      question: "What is the letter H indicating the electrode coding?",
+      immg: "img22",
+      answer: "Hydrogen controlled",
+      options: [
+        "Hydrogen controlled",
+        "Electrode efficiency",
+        "Current condition",
+        "Radio graphic quality"
+      ]
+    },
+    {
+      numb: 23,
+      question: "What is the use of calcium carbide?",
+      immg: "img23",
+      answer: "Produces acetylene gas",
+      options: [
+        "Produces acetylene gas",
+        "Produces oxygen gas",
+        "Produces nitrogen gas",
+        "Produces hydrogen gas"
+      ]
+    },
+    {
+      numb: 24,
+      question: "What is the percentage of calcium in calcium carbide?",
+      immg: "img24",
+      answer: "62.5%",
+      options: [
+        "50.2%",
+        "57.3%",
+        "62.5%",
+        "75.3%"
+      ]
+    },
+    {
+      numb: 25,
+      question: "What is the colour of liquid oxygen?",
+      immg: "img25",
+      answer: "Pale blue",
+      options: [
+        "Pale blue",
+        "Dark blue",
+        "Pale yellow",
+        "Dark yellow"
+      ]
+    },
+    {
+      numb: 26,
+      question: "At which temperature the liquid oxygen becomes solid?",
+      immg: "img26",
+      answer: "-218.4oC",
+      options: [
+        "-200oC",
+        "-205oC",
+        "-218.4oC",
+        "-325.3oC"
+      ]
+    },
+    {
+      numb: 27,
+      question: "What is percentage of oxygen in atmosphere?",
+      immg: "img27",
+      answer: "21%",
+      options: [
+        "21%",
+        "33%",
+        "45%",
+        "52%"
+      ]
+    },
+    {
+      numb: 28,
+      question: "What is the colour of welding oxygen cylinder?",
+      immg: "img28",
+      answer: "Black",
+      options: [
+        "Red",
+        "Blue",
+        "Black",
+        "Maroon"
+      ]
+    },
+    {
+      numb: 29,
+      question: "Which cylinder is fitted with safety fuse plug?",
+      immg: "img29",
+      answer: "Acetylene",
+      options: [
+        "Oxygen",
+        "Argon",
+        "Acetylene",
+        "Carbon-di-oxide"
+      ]
+    },
+    {
+      numb: 30,
+      question: "What is the capacity of welding an oxygen cylinder?",
+      immg: "img30",
+      answer: "7m3",
+      options: [
+        "5m3",
+        "7m3",
+        "10m3",
+        "15m3"
+      ]
+    }
+  
+  
+    {
+      numb: 31,
+      question: "Which is the name of the welding technique shown in the figure?",
+      immg: "img31",
+      answer: "Rightward",
+      options: [
+        "Leftward",
+        "Rightward",
+        "Forehand",
+        "Downward"
+      ]
+    },
+    {
+      numb: 32,
+      question: "Which type of blowpipe is used in the high pressure system?",
+      immg: "img32",
+      answer: "Non injector",
+      options: [
+        "Needle",
+        "Injector",
+        "Purifier",
+        "Non injector"
+      ]
+    },
+    {
+      numb: 33,
+      question: "In which welding technique, the welding starts at the right hand edge and moves towards the left?",
+      immg: "img33",
+      answer: "Leftward",
+      options: [
+        "Leftward",
+        "Rightward",
+        "Backward",
+        "Up and Downward"
+      ]
+    },
+    {
+      numb: 34,
+      question: "What is the full form of CCMS?",
+      immg: "img34",
+      answer: "Copper Coated Mild Steel",
+      options: [
+        "Copper Coated Mild Steel",
+        "Carbon Coated Mild Steel",
+        "Calcium Coated Mild Steel",
+        "Chromium Coated Mild Steel"
+      ]
+    },
+    {
+      numb: 35,
+      question: "What is the function of flux in Oxy-acetylene gas welding?",
+      immg: "img35",
+      answer: "Prevents oxidation",
+      options: [
+        "Prevents oxidation",
+        "Combustion of metal",
+        "Strengthens filler rod",
+        "Strengthens electrode"
+      ]
+    },
+    {
+      numb: 36,
+      question: "What is the name of the brazing shown in the figure?",
+      immg: "img36",
+      answer: "Torch brazing",
+      options: [
+        "Dip brazing",
+        "Torch brazing",
+        "Furnace brazing",
+        "Induction brazing"
+      ]
+    },
+    {
+      numb: 37,
+      question: "What is the name of the part marked as X shown in the figure?",
+      immg: "img37",
+      answer: "Soldering iron",
+      options: [
+        "Base metal",
+        "Copper tip",
+        "Soldering iron",
+        "Soldered seam"
+      ]
+    },
+    {
+      numb: 38,
+      question: "What is the name of the defect marked as X shown in the figure?",
+      immg: "img38",
+      answer: "Blow holes",
+      options: [
+        "Cracks",
+        "Porosity",
+        "Blow holes",
+        "Burn through"
+      ]
+    },
+    {
+      numb: 39,
+      question: "What is the S.I unit of heat?",
+      immg: "img39",
+      answer: "Joule",
+      options: [
+        "Calorie",
+        "Joule",
+        "Centigrade heat unit",
+        "British thermal unit"
+      ]
+    },
+    {
+      numb: 40,
+      question: "What is the value for specific heat of water?",
+      immg: "img40",
+      answer: "1",
+      options: [
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    },
+    {
+      numb: 41,
+      question: "What is the boiling point of water?",
+      immg: "img41",
+      answer: "100oC",
+      options: [
+        "0oC",
+        "32oC",
+        "100oC",
+        "212oC"
+      ]
+    },
+    {
+      numb: 42,
+      question: "Which is the unit of current?",
+      immg: "img42",
+      answer: "Ampere",
+      options: [
+        "Ampere",
+        "Volt",
+        "Ohm",
+        "Watt"
+      ]
+    },
+    {
+      numb: 43,
+      question: "What is the flow of electrons in any conductor?",
+      immg: "img43",
+      answer: "Current",
+      options: [
+        "Voltage",
+        "Current",
+        "Resistance",
+        "Power"
+      ]
+    },
+    {
+      numb: 44,
+      question: "What is the side of a square whose area is 625mm2?",
+      immg: "img44",
+      answer: "25 mm",
+      options: [
+        "15 mm",
+        "20 mm",
+        "25 mm",
+        "30 mm"
+      ]
+    },
+    {
+      numb: 45,
+      question: "Identify the name of tool?",
+      immg: "img45",
+      answer: "Try square",
+      options: [
+        "Try square",
+        "Surface gauge",
+        "Steel rule",
+        "Feeler gauge"
+      ]
+    },
+    {
+      numb: 46,
+      question: "Identify the name of tool?",
+      immg: "img46",
+      answer: "Ball pein hammer",
+      options: [
+        "Ball pein hammer",
+        "Cross pein hammer",
+        "Straight pein hammer",
+        "Sledge hammer"
+      ]
+    },
+    {
+      numb: 47,
+      question: "Identify the name of tool?",
+      immg: "img47",
+      answer: "Cutting plier",
+      options: [
+        "Hawk-billed shear",
+        "Aviation shear",
+        "Cutting plier",
+        "Double cutting shear"
+      ]
+    },
+    {
+      numb: 48,
+      question: "Identify the name of tool?",
+      immg: "img48",
+      answer: "Mallet",
+      options: [
+        "Ball pein hammer",
+        "Mallet",
+        "Cross pein hammer",
+        "Staright pein hammer"
+      ]
+    },
+    {
+      numb: 49,
+      question: "Identify the name of tool?",
+      immg: "img49",
+      answer: "Open end spanner",
+      options: [
+        "Open end spanner",
+        "Ring spanner",
+        "Combination spanner",
+        "Adjustable spanner"
+      ]
+    },
+    {
+      numb: 50,
+      question: "Which dimension correctly marked in the circle as per standard?",
+      immg: "img50",
+      answer: "A2",
+      options: [
+        "A1",
+        "A2",
+        "A3",
+        "A4"
+      ]
+    }
+  
+  
+];
